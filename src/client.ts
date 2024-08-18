ffa3e33e2016bfc95eb9e2587f6b962e60aac16f
ted <theo_drz@live.com>
2024-08-17 23:40:47 -0400

    setup basic websocket connection with gameCodes

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,17 @@
 import t from '@/lib/getTag'
 
+let ws: WebSocket;
+
 document.body.appendChild(
-  t('h1', { textContent: 'hello world', className: 'bg-red-500 text-white' })
+  t('button', {
+    textContent: 'Join General Lobby',
+    className: 'm-4 p-4 border-4 border-black',
+    onclick: () => {
+      const { host, protocol } = window.location
+      ws = new WebSocket(`${protocol === 'http:' ? 'ws' : 'wss'}://${host}?gameCode=general`)
+      ws.onmessage = (ws) => {
+        console.log(ws.data)
+      }
+    }
+  })
 )
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
+import type { ServerWebSocket } from 'bun';
+
+interface ClientData {
+  gameCode: string
+}
+
+type ClientSocket = ServerWebSocket<ClientData>;
+
 await Bun.build({
   entrypoints: [ 'src/client.ts' ],
   outdir: 'public/',
@@ -7,16 +15,39 @@ await Bun.build({
 
 Bun.spawnSync('bunx tailwindcss -i src/globals.css -o public/style.css --minify'.split(' '))
 
-Bun.serve({
-  fetch: async (req) => {
-    let path = new URL(req.url).pathname;
-    if (path === '/') path = '/index.html';
-    console.log(path);
-    const file = Bun.file('public' + path);
+const allGames: { [key: string]: ClientSocket[] } = {}
+
+Bun.serve<ClientData>({
+  fetch: async (req, server) => {
+    console.log(new URL(req.url))
+    let { pathname, searchParams } = new URL(req.url);
+    if (server.upgrade(req, { data: { gameCode: searchParams.get('gameCode') } })) return
+
+    if (pathname === '/') pathname = '/index.html';
+    console.log(pathname);
+    const file = Bun.file('public' + pathname);
     const exists = await file.exists();
     return new Response(
       exists ? file : 'File not found',
       { status: exists ? 200 : 404 }
     )
+  },
+  websocket: {
+    message: (ws, msg) => {
+      console.log(msg)
+    },
+    open: (ws) => {
+      console.log('OPENED', ws)
+      const game = allGames[ws.data.gameCode];
+      if (game) {
+        game.push(ws)
+      } else {
+        allGames[ws.data.gameCode] = [ ws ]
+      }
+      ws.send(`connected to ${ws.data.gameCode}`)
+    },
+    close: (ws, code, reason) => {
+      console.log('CLOSED')
+    }
   }
 });
